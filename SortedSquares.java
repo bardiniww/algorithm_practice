d59f0c1d815dd465633adfb33168f7b723162dee
bardiniww <bardiniww@gmail.com>
2022-12-20 19:36:34 +0800

    SortedSquares added

---
+++ SortedSquares.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class SortedSquares {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(sortedSquares(new int[]{-39, -7, -2, 0, 1, 10, 13})));
+    }
+
+    public static int[] sortedSquares(int[] nums) {
+        int size = nums.length;
+        int[] res = new int[size];
+
+        int left = 0;
+        int right = size -1;
+
+        for (int i = size - 1; i != 0; i--) {
+            int item;
+            if (Math.abs(nums[right]) > Math.abs(nums[left])) {
+                item = nums[right];
+                right--;
+            }
+            else {
+                item = nums[left];
+                left++;
+            }
+            res[i] = item * item;
+        }
+
+        return res;
+    }
+}
